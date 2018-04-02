662b2e5a283a80b0fab4ff1c12a007fa17a6cd08
Ke Nguyen <ke.nguyen@2359media.com>
2018-04-02 15:57:09 +0700

    try restarting application

--- scripts/code-deploy/application-start.sh
+++ scripts/code-deploy/application-start.sh
@@ -4,5 +4,5 @@
 cd /home/ec2-user/app
 
 pm2=$(which pm2)
-cd /home/ec2-user/app/packages/api && $pm2 -f start server.js
-cd /home/ec2-user/app/packages/cms && $pm2 -f start server.js
+cd /home/ec2-user/app/packages/api && $pm2 restart server.js
+cd /home/ec2-user/app/packages/cms && $pm2 restart server.js
