9ee43ca3b42c7ad05117bb55edfb7fbaeecc2460
Ke Nguyen <ke.nguyen@2359media.com>
2018-04-02 13:35:46 +0700

    load bash config when run deploy hooks

--- scripts/code-deploy/after-install.sh
+++ scripts/code-deploy/after-install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+. /home/ec2-user/.bashrc
+
 npm=$(which npm)
 $npm install
 ./node_modules/.bin/lerna bootstrap
--- scripts/code-deploy/application-start.sh
+++ scripts/code-deploy/application-start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+. /home/ec2-user/.bashrc
+
 NPM=$(which npm)
 cd /var/app/packages/api && $NPM start
 cd /var/app/packages/cms && $NPM start
